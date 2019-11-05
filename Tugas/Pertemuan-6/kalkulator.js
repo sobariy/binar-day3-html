fca9ec872338c7e955bcd0b9c4fdeb15cf40e526
Sobariy <sobariycoc@gmail.com>
2019-11-05 10:53:57 +0700

    [makruf] - edit Kalkulator.js , tugas.js

--- Tugas/Pertemuan-6/kalkulator.js
+++ Tugas/Pertemuan-6/kalkulator.js
@@ -1,29 +1,31 @@
-
-function kalkulator(oper, angka1, angka2){
-    switch (oper) {
-        case "pertambahan":
-            return angka1 + angka2;
+// define parameter oper, angka1, angka2
+let kalkulator = (oper, angka1, angka2) => {
+    switch (oper) { //switch case
+        case `pertambahan`:
+            text = `Hasil dari ${oper} ${angka1} dan ${angka2} adalah ${angka1 + angka2}`;
             break;
-        case "pengurangan":
-            return angka1 - angka2;
+        case `pengurangan`:
+            text = `Hasil dari ${oper} ${angka1} dan ${angka2} adalah ${angka1 - angka2}`;
             break;
-        case "perkalian":
-            return angka1 * angka2;
+        case `perkalian`:
+            text = `Hasil dari ${oper} ${angka1} dan ${angka2} adalah ${angka1 * angka2}`;
             break;
-        case "eksponensial":
-            return angka1 % angka2;
+        case `eksponensial`:
+            text = `Hasil dari ${oper} ${angka1} dan ${angka2} adalah ${angka1 % angka2}`;
             break;
-        case "pembagian":
-            return angka1 / angka2;
+        case `pembagian`:
+            text = `Hasil dari ${oper} ${angka1} dan ${angka2} adalah ${angka1 / angka2}`;
             break;
         default:
-            return "yang benerlah";
+            text = `yang bener dongs`;
     }
+    return text;
 }
+ console.log (kalkulator('pertambahan', 10, 5));
+ console.log (kalkulator("pengurangan", 10, 5))
+ console.log (kalkulator("pembagian", 10, 5))
+ console.log (kalkulator("eksponensial", 10, 5))
+ console.log (kalkulator("perkalian", 10, 5))
+ console.log (kalkulator(10, 5, "pembagian"))
 
-console.log (kalkulator("pertambahan", 10, 5))
-console.log (kalkulator("pengurangan", 10, 5))
-console.log (kalkulator("pembagian", 10, 5))
-console.log (kalkulator("eksponensial", 10, 5))
-console.log (kalkulator("perkalian", 10, 5))
-console.log (kalkulator(10, 5, "pembagian"))
+
--- Tugas/Pertemuan-6/tugas.js
+++ Tugas/Pertemuan-6/tugas.js
@@ -10,10 +10,12 @@
 
 // console.log(myProfile("Bambang", 66, "Memancing"));
 
-let myProfile (nama, umur, hobi) => { 
+let myProfile = (nama, umur, hobi) => { 
     if (umur <= 56) {
-    console.log ("Nama saya " + nama + ". Saya masih aktif bekerja. Umur saya " + umur + " Tahun. Saya Hobi " + hobi); 
+    console.log (`Nama saya ${nama}. Saya masih aktif bekerja. Umur saya ${umur} Tahun. Saya Hobi ${hobi}`); 
 } else {
-    console.log("Nama saya " + nama + ". Saya sudah pensiun. Umur saya " + umur + " Tahun. Saya Hobi " + hobi); 
+    console.log (`Nama saya ${nama}. Saya sudah pensiun bekerja. Umur saya ${umur} Tahun. Saya Hobi ${hobi}`); 
 }
 }
+
+console.log(myProfile(`Bambang`, 55, `Tidur`))
